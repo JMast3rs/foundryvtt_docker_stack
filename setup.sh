cf66c6d8c3e0a38085d7c0dd3b711ee92c1e2dbc
JMast3rs <james@mast3rs.com>
2022-02-22 15:19:25 +0000

    Missed 1 sudo!

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get upgrade -y
 
 apt-get remove docker docker-engine docker.io containerd runc -y
 apt-get install ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
